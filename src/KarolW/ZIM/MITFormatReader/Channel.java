98d8bbbd7679c0da2cb931b1df77221cdf8ba495
Karol Wyskocki <karol.wyskocki@vp.pl>
2017-06-03 02:09:19 +0200

    Created classes of MIT format reader.

---
+++ src/KarolW/ZIM/MITFormatReader/Channel.java
@@ -0,0 +1,118 @@
+package KarolW.ZIM.MITFormatReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by Karol on 02-06-2017.
+ */
+public class Channel {
+
+    private String filePath;
+    private String formatName;
+    private int gain;
+    private int resolution;
+    private String description;
+    private int byteOffset;
+    private int channelsInFile;
+    private int length;
+
+    public Channel(String filePath, String formatName, int gain, int resolution,
+                   String description, int byteOffset, int channelsInFile, int length) {
+        this.filePath = filePath;
+        this.formatName = formatName;
+        this.gain = gain;
+        this.resolution = resolution;
+        this.description = description;
+        this.byteOffset = byteOffset;
+        this.length = length;
+        this.channelsInFile = channelsInFile;
+    }
+
+
+    public int[] getRawData() throws IOException {
+        int[] rawData = new int[length];
+        File dataFile = new File(filePath);
+        FileInputStream fis = new FileInputStream(dataFile);
+
+
+
+        byte[] sample = new byte[2];
+        int sampleNumber = 0;
+
+        for (int i = 0; i < length * channelsInFile; i++) {
+            if(fis.read(sample) == -1){
+                break;
+            }
+            if ((i % channelsInFile) == byteOffset) {
+
+                if ((sample[1] & (0x80)) == 0x80) {
+                    rawData[sampleNumber++] = (0xFFFF0000 | ((sample[1] & 0xFF) << 8) | ((sample[0] & 0xFF)));
+                } else {
+                    rawData[sampleNumber++] = ((sample[1]) << 8) | ((sample[0]));
+                }
+            }
+        }
+
+        return rawData;
+    }
+
+    public double[] getData() throws IOException {
+        int[] rawData = getRawData();
+        double[] data = new double[rawData.length];
+
+        for (int i = 0; i < data.length; i++){
+            data[i] = (double)rawData[i]/gain;
+        }
+        return data;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getFormatName() {
+        return formatName;
+    }
+
+    public int getGain() {
+        return gain;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getByteOffset() {
+        return byteOffset;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getChannelsInFile() {
+        return channelsInFile;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "filePath='" + filePath + '\'' +
+                ", formatName='" + formatName + '\'' +
+                ", gain=" + gain +
+                ", resolution=" + resolution +
+                ", description='" + description + '\'' +
+                ", byteOffset=" + byteOffset +
+                ", channelsInFile=" + channelsInFile +
+                ", length=" + length +
+                '}';
+    }
+}
---
+++ src/KarolW/ZIM/MITFormatReader/RecordMIT.java
@@ -0,0 +1,112 @@
+package KarolW.ZIM.MITFormatReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by Karol on 02-06-2017.
+ */
+public class RecordMIT {
+
+    private String filePath;
+    private String recordName;
+    private int samplingFrequency;
+    private int numberOfSample;
+    private String info;
+    private Channel[] channels;
+
+
+    public RecordMIT(String pathName) throws FileNotFoundException {
+        filePath = pathName;
+        File headerFile = new File(filePath);
+        FileInputStream fis = new FileInputStream(headerFile);
+        Scanner fileScanner = new Scanner(fis);
+
+        HashMap<String, Integer> chInFile = new HashMap<>();
+
+        String firstLine [] = fileScanner.nextLine().split(" ");
+
+        this.recordName = firstLine[0];
+        int numberOfChannel = Integer.parseInt(firstLine[1]);
+        this.samplingFrequency = Integer.parseInt(firstLine[2]);
+        this.numberOfSample = Integer.parseInt(firstLine[3]);
+
+        channels = new Channel[numberOfChannel];
+        String[] channelLine = new String[channels.length];
+
+        for(int i = 0; i < channelLine.length; i++){
+            if(!fileScanner.hasNext()){
+                break;
+            }
+            channelLine[i] = fileScanner.nextLine();
+            String fileName = channelLine[i].split(" ")[0].trim();
+            chInFile.put(fileName, chInFile.containsKey(fileName) ? chInFile.get(fileName)+1 : 1);
+        }
+
+        StringBuilder infoBuilder = new StringBuilder();
+        while (fileScanner.hasNextLine()) {
+            infoBuilder.append(fileScanner.nextLine()+System.lineSeparator());
+        }
+        info = infoBuilder.toString();
+        fileScanner.close();
+
+        int offset = 0;
+        for(int i = 0; i < channels.length; i++){
+            String[] channelInfo = channelLine[i].split(" ");
+            StringBuilder desc = new StringBuilder();
+            for(int j = 8; j < channelInfo.length; j++){
+                desc.append(channelInfo[j]+" ");
+            }
+            if(offset >= chInFile.get(channelInfo[0])){
+                offset = 0;
+            }
+            channels[i] = new Channel(
+                    headerFile.getParent() + File.separator + channelInfo[0],
+                    channelInfo[1],
+                    Integer.parseInt(channelInfo[2]),
+                    Integer.parseInt(channelInfo[3]),
+                    desc.toString(),
+                    offset++,
+                    chInFile.get(channelInfo[0]),
+                    numberOfSample);
+        }
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getRecordName() {
+        return recordName;
+    }
+
+    public int getSamplingFrequency() {
+        return samplingFrequency;
+    }
+
+    public int getNumberOfSample() {
+        return numberOfSample;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public Channel[] getChannels() {
+        return channels;
+    }
+
+    @Override
+    public String toString() {
+        return "RecordMIT{" +
+                "filePath='" + filePath + '\'' +
+                ", recordName='" + recordName + '\'' +
+                ", samplingFrequency=" + samplingFrequency +
+                ", numberOfSample=" + numberOfSample +
+                ", info='" + info + '\'' +
+                '}';
+    }
+}
