9d87f42f18119b2b207bdc644741989fc753a011
Karol Wyskocki <karol.wyskocki@vp.pl>
2017-06-18 17:03:44 +0200

    The decision function of QRS detection was implemented.

--- src/KarolW/ZIM/QRSdetect/QRSdetector.java
+++ src/KarolW/ZIM/QRSdetect/QRSdetector.java
@@ -3,29 +3,63 @@ package KarolW.ZIM.QRSdetect;
 
 import KarolW.ZIM.DSP.MeanFilter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Karol on 03-06-2017.
  */
 public class QRSdetector {
 
-    private double[] data;
-    private int meanWindowWidth;
-    private double decisionThreshold;
 
-    public QRSdetector(double[] data, int meanWindowWidth, double decisionThreshold) {
-        this.data = data;
-        this.meanWindowWidth = meanWindowWidth;
+    private double decisionThreshold;
+    private int samplingFreq;
+
+
+    public QRSdetector(int samplingFreq, double decisionThreshold) {
+        this.samplingFreq = samplingFreq;
         this.decisionThreshold = decisionThreshold;
     }
 
-    public double[] process(){
-        return detectFunction(data);
+    public int[] detectQRS(double[] data){
+        return decisionFunction(detectFunction(data));
+    }
+
+    private int [] decisionFunction(double[] data){
+        int[] result = new int[data.length];
+        Set<Integer> qrsSampleNumbers = new HashSet<Integer>();
+        int lastMaxIndex = Integer.MIN_VALUE;
+
+        int detectWindowSize = samplingFreq/4; //detectWindowSize span 250 ms of signal
+        int maxSpace = (int)(samplingFreq*0.35); //space between maxims has 350 ms
+
+        for (int i = 0; i < data.length-detectWindowSize; i++) {
+            int maxIndex = getMaxValueIndex(data, i, i+detectWindowSize);
+            if(data[maxIndex] >= decisionThreshold){
+                i = maxIndex;
+                if (maxIndex - lastMaxIndex > maxSpace) {
+                    if(lastMaxIndex >= 0) {
+                        qrsSampleNumbers.add(lastMaxIndex);
+                    }
+                    lastMaxIndex = maxIndex;
+                }else{
+                    if((lastMaxIndex >= 0 ? data[lastMaxIndex] : Integer.MIN_VALUE) < data[maxIndex]){
+                        lastMaxIndex = maxIndex;
+                    }
+                }
+            }
+        }
+        for (Integer sampleNumber : qrsSampleNumbers) {
+            result[sampleNumber] = 1;
+        }
+
+        return result;
     }
 
     private double[] detectFunction(double[] data){
-        MeanFilter meanFilter = new MeanFilter(meanWindowWidth);
+        MeanFilter meanFilter = new MeanFilter(samplingFreq > 100 ? samplingFreq/5 : 8);
 
-        double [] detectFcnData = derivate(data);
+        double [] detectFcnData = derivative(data);
 
         for (int i = 0; i < detectFcnData.length; i++) {
             detectFcnData[i] = detectFcnData[i]*detectFcnData[i];
@@ -54,17 +88,51 @@ public class QRSdetector {
         return max;
     }
 
-    private double[] derivate(double[] data){
-        double[] derivatedData = new double[data.length];
+    private int getMaxValueIndex(double[] data){
+        int index = 0;
+        double max = Double.NEGATIVE_INFINITY;
+
+        for (int i = 0; i < data.length; i++) {
+            if(data[i] > max){
+                max = data[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private int getMaxValueIndex(double[] data, int from, int to){
+        int index = 0;
+        double max = Double.NEGATIVE_INFINITY;
+
+        if (from > to){
+            throw new IllegalArgumentException(from + " > " + to);
+        }
+
+        if(data.length < to){
+            to = data.length;
+        }
+
+        for (int i = from; i < to; i++) {
+            if(data[i] > max){
+                max = data[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private double[] derivative(double[] data){
+        double[] results = new double[data.length];
 
         for (int i = 0; i < data.length; i++) {
             double a = data[i-2 < 0 ? 0 : i-2];
             double b = data[i-1 < 0 ? 0 : i-1];
             double c = data[i+1 > data.length-1 ? data.length-1: i+1];
             double d = data[i+2 > data.length-1 ? data.length-1: i+2];
-            derivatedData[i] = (a - 8*b + 8*c -d)/12.0;
+            results[i] = (a - 8*b + 8*c -d)/12.0;
         }
-        return derivatedData;
+        return results;
     }
 
     private double mean(double[] data){
@@ -74,4 +142,21 @@ public class QRSdetector {
         }
         return sum/data.length;
     }
+
+    public double getDecisionThreshold() {
+        return decisionThreshold;
+    }
+
+    public void setDecisionThreshold(double decisionThreshold) {
+        this.decisionThreshold = decisionThreshold;
+    }
+
+    public int getSamplingFreq() {
+        return samplingFreq;
+    }
+
+    public void setSamplingFreq(int samplingFreq) {
+        this.samplingFreq = samplingFreq;
+    }
 }
+
